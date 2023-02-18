3012355704bfc92ed9205d013ccd68b26db3b6ec
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2023-02-18 11:44:59 -0500

    Added Navbar / Nuxt routing

---
+++ pages/display.vue
@@ -0,0 +1,18 @@
+<style lang="scss">
+@import "./node_modules/bulma/bulma.sass";
+</style>
+<template>
+  <div class="container">
+    <div class="section">
+      <div class="columns">
+        <div class="column">
+          <PhotoGallery />
+        </div>
+        <div class="column">
+          <ToDoViewer />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup></script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,7 @@
 <style lang="scss">
 @import "./node_modules/bulma/bulma.sass";
 </style>
-
 <template>
-  <TheNavbar />
-  <NuxtPage />
+  <h1>This is home</h1>
 </template>
-
 <script setup></script>
